6f6d44537bf9b0627c6edca161d342fb0182fd3e
Petr Lobachev <petya.lobachev@yandex.ru>
2021-03-01 10:53:31 +0000

    feat: поправили baseurl в auth.js

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://auth.nomoreparties.co';
+export const BASE_URL = 'https://api.lobachev.students.nomoreparties.space';
 
 const checkresponse = (res) => (res.ok ? res.json() : Promise.reject(`Error: ${res.status}`));
 
