2cf2f523145661d9fb6f34584aaa226ae30cfdfa
Petr Lobachev <petya.lobachev@yandex.ru>
2021-03-01 20:25:21 +0300

    fix: удалить свойстао авторизаци

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -116,7 +116,6 @@ class Api {
 const api = new Api({
   baseUrl: 'https://api.lobachev.students.nomoreparties.space',
   headers: {
-    authorization: 'fa53dc10-0bed-404e-84a8-68518d7f0629',
     'Content-Type': 'application/json',
   },
 });
