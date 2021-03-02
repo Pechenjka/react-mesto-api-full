89746705b35c0e4216af7b2d7ec31a074be0e2cb
Petr Lobachev <petya.lobachev@yandex.ru>
2021-03-02 23:56:39 +0300

    fix: исправить populate

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -3,7 +3,7 @@ const Card = require('../models/card');
 
 const getCards = (req, res, next) => {
   Card.find({})
-    .populate(['owner', 'likes'])
+    .populate('owner', 'likes')
     .then((cards) => res.send(cards))
     .catch(next);
 };
